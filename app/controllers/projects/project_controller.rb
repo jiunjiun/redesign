488d34739ea59b846ce2ecbaa547827fdf186fc1
jiunjiun <quietmes@gmail.com>
2014-12-05 04:51:33 +0800

    修正新增專案的問題

--- app/controllers/projects/project_controller.rb
+++ app/controllers/projects/project_controller.rb
@@ -1,5 +1,6 @@
-class Projects::ProjectController < Projects::AccessController
-  before_filter :set_project, only: [:edit, :update, :destroy]
+class Projects::ProjectController < ApplicationController
+  before_action :set_project, only: [:edit, :update, :destroy]
+  before_action :user_access
 
   def new
     @project = Project.new
@@ -15,6 +16,10 @@ class Projects::ProjectController < Projects::AccessController
   end
 
   private
+    def user_access
+      render_404 unless user_signed_in?
+    end
+
     def set_project
       @project = Project.find_by({user: @current_page_user, name: params[:project_name]})
       redirect_to home_path(params[:username]) unless @project
